ae9e1c39409a407ca88f3345269ed960a8b66eb7
vanphan <vanthuyphan@gmail.com>
2016-05-19 22:33:18 +0700

    refactor

--- bin/mailer.js
+++ bin/mailer.js
@@ -16,28 +16,19 @@ exports.init = function (_now, cb) {
             pass: now.ini.gmail.password
         }
     });
-
-
     cb();
 }
 
 mailer.sendMail = function (data ,template, cb) {
     var template = process.cwd() + '/views/email-templates/' + template + '.jade';
-
-    // get template from file system
     fs.readFile(template, 'utf8', function(err, file){
         if(err){
-            //handle errors
             console.log('ERROR!');
             cb(err);
         }
         else {
-            //compile jade template into function
             var compiledTmpl = jade.compile(file, {filename: template});
-            // set context to be used in template
-            var context = {"email": data.email, "code": data.code};
-            // get html back as a string with the context applied;
-            var html = compiledTmpl(context);
+            var html = compiledTmpl(data);
 
             mailOptions = {
                 from: 'cafe2sales@gmail.com',
