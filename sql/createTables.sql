4bdc80771ac5c77f2473c412b7ebb61c5b81098d
vanthuyphan <vanthuyphan@gmail.com>
2016-05-21 20:44:42 +0700

    fixed #9: add Area

--- sql/createTables.sql
+++ sql/createTables.sql
@@ -58,3 +58,79 @@ INSERT INTO Category (name) VALUES ('Văn phòng phẩm');
 INSERT INTO Category (name) VALUES ('Dược Phẩm Y tế');
 INSERT INTO Category (name) VALUES ('Ô tô-Xe máy');
 INSERT INTO Category (name) VALUES ('Khác');
+
+CREATE TABLE IF NOT EXISTS `Area` (
+	`code` BIGINT NOT NULL AUTO_INCREMENT,
+	`name` NVARCHAR (100) UNIQUE,
+	PRIMARY KEY (`code`)
+) ENGINE MyISAM DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+
+INSERT INTO Area (name) VALUES ('Hà Nội');
+INSERT INTO Area (name) VALUES ('Hồ Chí Minh');
+INSERT INTO Area (name) VALUES ('Đà Nẵng');
+INSERT INTO Area (name) VALUES ('Bắc Giang');
+INSERT INTO Area (name) VALUES ('Bắc Kạn');
+INSERT INTO Area (name) VALUES ('Bắc Ninh');
+INSERT INTO Area (name) VALUES ('Cao Bằng');
+INSERT INTO Area (name) VALUES ('Điện Biên');
+INSERT INTO Area (name) VALUES ('Hà Giang');
+INSERT INTO Area (name) VALUES ('Hà Nam');
+INSERT INTO Area (name) VALUES ('Hải Dương');
+INSERT INTO Area (name) VALUES ('Hải Phòng');
+INSERT INTO Area (name) VALUES ('Hòa Bình');
+INSERT INTO Area (name) VALUES ('Hưng Yên');
+INSERT INTO Area (name) VALUES ('Lai Châu');
+INSERT INTO Area (name) VALUES ('Lào Cai');
+INSERT INTO Area (name) VALUES ('Lạng Sơn');
+INSERT INTO Area (name) VALUES ('Nam Định');
+INSERT INTO Area (name) VALUES ('Ninh Bình');
+INSERT INTO Area (name) VALUES ('Phú Thọ');
+INSERT INTO Area (name) VALUES ('Quảng Ninh');
+INSERT INTO Area (name) VALUES ('Sơn La');
+INSERT INTO Area (name) VALUES ('Thái Nguyên');
+INSERT INTO Area (name) VALUES ('Thái Bình');
+INSERT INTO Area (name) VALUES ('Tuyên Quang');
+INSERT INTO Area (name) VALUES ('Vĩnh Phúc');
+INSERT INTO Area (name) VALUES ('Yên Bái');
+INSERT INTO Area (name) VALUES ('Bình Định');
+INSERT INTO Area (name) VALUES ('Đăk Lăk');
+INSERT INTO Area (name) VALUES ('Đắc Nông');
+INSERT INTO Area (name) VALUES ('Gia Lai');
+INSERT INTO Area (name) VALUES ('Hà Tĩnh');
+INSERT INTO Area (name) VALUES ('Khánh Hòa');
+INSERT INTO Area (name) VALUES ('Kon Tum');
+INSERT INTO Area (name) VALUES ('Nghệ An');
+INSERT INTO Area (name) VALUES ('Phú Yên');
+INSERT INTO Area (name) VALUES ('Quảng Bình');
+INSERT INTO Area (name) VALUES ('Quảng Nam');
+INSERT INTO Area (name) VALUES ('Quảng Ngãi');
+INSERT INTO Area (name) VALUES ('Quảng Trị');
+INSERT INTO Area (name) VALUES ('Thanh Hóa');
+INSERT INTO Area (name) VALUES ('Thừa Thiên Huế');
+INSERT INTO Area (name) VALUES ('An Giang');
+INSERT INTO Area (name) VALUES ('Bà Rịa-Vũng Tàu');
+INSERT INTO Area (name) VALUES ('Bạc Liêu');
+INSERT INTO Area (name) VALUES ('Bến Tre');
+INSERT INTO Area (name) VALUES ('Bình Dương');
+INSERT INTO Area (name) VALUES ('Bình Phước');
+INSERT INTO Area (name) VALUES ('Bình Thuận');
+INSERT INTO Area (name) VALUES ('Cà Mau');
+INSERT INTO Area (name) VALUES ('Cần Thơ');
+INSERT INTO Area (name) VALUES ('Công nghiệp');
+INSERT INTO Area (name) VALUES ('Đồng Nai');
+INSERT INTO Area (name) VALUES ('Đồng Tháp');
+INSERT INTO Area (name) VALUES ('Hậu Giang');
+INSERT INTO Area (name) VALUES ('Kiên Giang');
+INSERT INTO Area (name) VALUES ('Lâm Đồng');
+INSERT INTO Area (name) VALUES ('Long An');
+INSERT INTO Area (name) VALUES ('Ninh Thuận');
+INSERT INTO Area (name) VALUES ('Sóc Trăng');
+INSERT INTO Area (name) VALUES ('Tây Ninh');
+INSERT INTO Area (name) VALUES ('Tiền Giang');
+INSERT INTO Area (name) VALUES ('Trà Vinh');
+INSERT INTO Area (name) VALUES ('Vĩnh Long');
+INSERT INTO Area (name) VALUES ('Nước ngoài');
+INSERT INTO Area (name) VALUES ('Toàn Quốc');
+INSERT INTO Area (name) VALUES ('Shop Online');
+
+
