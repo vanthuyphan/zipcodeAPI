0cdd7538d5b0ea892cf4044c093162c4d9c65c7c
vanphan <vanthuyphan@gmail.com>
2016-05-21 20:10:45 +0700

    #merge

--- sql/createTables.sql
+++ sql/createTables.sql
@@ -30,3 +30,31 @@ CREATE TABLE IF NOT EXISTS `Oauth` (
 
 
 INSERT INTO `User` VALUES (1, 'v4n', 'van@gmail.com', 'Van Phan', 0, '12345', '12345', 0);
+
+CREATE TABLE IF NOT EXISTS `Category` (
+	`code` BIGINT NOT NULL AUTO_INCREMENT,
+	`name` NVARCHAR (100) UNIQUE,
+	PRIMARY KEY (`code`)
+) ENGINE MyISAM DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+
+
+INSERT INTO Category (name) VALUES ('Tiêu dùng');
+INSERT INTO Category (name) VALUES ('Thời trang');
+INSERT INTO Category (name) VALUES ('Dịch vụ');
+INSERT INTO Category (name) VALUES ('Công nghiệp');
+INSERT INTO Category (name) VALUES ('Thực phẩm');
+INSERT INTO Category (name) VALUES ('Mỹ Phẩm');
+INSERT INTO Category (name) VALUES ('Máy tính-CNTT');
+INSERT INTO Category (name) VALUES ('Nông Lâm nghiệp');
+INSERT INTO Category (name) VALUES ('Đồ Uống');
+INSERT INTO Category (name) VALUES ('Đồ chơi-Quà tặng');
+INSERT INTO Category (name) VALUES ('Viễn thông');
+INSERT INTO Category (name) VALUES ('Xây dựng');
+INSERT INTO Category (name) VALUES ('Mẹ và Bé');
+INSERT INTO Category (name) VALUES ('Thủ công mỹ nghệ');
+INSERT INTO Category (name) VALUES ('Điện máy');
+INSERT INTO Category (name) VALUES ('Nội-Ngoại thất');
+INSERT INTO Category (name) VALUES ('Văn phòng phẩm');
+INSERT INTO Category (name) VALUES ('Dược Phẩm Y tế');
+INSERT INTO Category (name) VALUES ('Ô tô-Xe máy');
+INSERT INTO Category (name) VALUES ('Khác');
